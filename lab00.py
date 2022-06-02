04098dae071a28a4fbab3af2e798708424c8312d
Mini-danger <73896686+Will-xyz2002@users.noreply.github.com>
2022-06-02 23:30:19 +0800

    Update lab00.py

--- lab00.py
+++ lab00.py
@@ -1,8 +1,8 @@
-def twenty_eighteen(a,b):
+def twenty_eighteen(a,b):       # You just defined a function called twenty_eighteen, and it has two arguments(parameters): a and b
     """Come up with the most creative expression that evaluates to 2018,
     using only numbers and the +, *, and - operators.
 
     >>> twenty_eighteen(2 , 1009) # 2 and 1009 are both prime numbers, just for fun
     2018
     """
-    return a * b
+    return a * b                # this is the function's output
