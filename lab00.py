3774913e63f879ce7f0b34c5ddc61603efe1a00f
Mini-danger <73896686+Will-xyz2002@users.noreply.github.com>
2022-06-02 23:28:13 +0800

    Update lab00.py

--- lab00.py
+++ lab00.py
@@ -2,7 +2,7 @@ def twenty_eighteen(a,b):
     """Come up with the most creative expression that evaluates to 2018,
     using only numbers and the +, *, and - operators.
 
-    >>> twenty_eighteen(2 , 1009)
+    >>> twenty_eighteen(2 , 1009) # 2 and 1009 are both prime numbers, just for fun
     2018
     """
     return a * b
