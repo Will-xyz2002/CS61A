3d42b043adc80161da74372b1be0b85a58a50bf7
Mini-danger <73896686+Will-xyz2002@users.noreply.github.com>
2022-06-03 20:20:58 +0800

    Add files via upload

---
+++ lab/lab00.py
@@ -0,0 +1,8 @@
+def twenty_eighteen(a,b):
+    """Come up with the most creative expression that evaluates to 2018,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_eighteen(2 , 1009)
+    2018
+    """
+    return a * b
