bd7e86aa46baa4ed925702d51c8de5f96665055a
Mini-danger <73896686+Will-xyz2002@users.noreply.github.com>
2022-06-03 20:22:07 +0800

    Update lab00.py

--- lab/lab00.py
+++ lab/lab00.py
@@ -1,8 +1,12 @@
-def twenty_eighteen(a,b):
+def twenty_eighteen():       # You just defined a function called twenty_eighteen, and it has two arguments(parameters): a and b
     """Come up with the most creative expression that evaluates to 2018,
     using only numbers and the +, *, and - operators.
-
-    >>> twenty_eighteen(2 , 1009)
+    >>> twenty_eighteen() 
     2018
     """
-    return a * b
+    
+    a = 2
+    b = 1009
+
+
+    return a * b      # this is the function's output
