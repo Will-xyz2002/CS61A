1d8d6ac929ff4eec8421b521696af50479458c31
Mini-danger <73896686+Will-xyz2002@users.noreply.github.com>
2022-06-03 20:28:28 +0800

    Update lab00.py

--- lab/lab00.py
+++ lab/lab00.py
@@ -1,12 +1,17 @@
-def twenty_eighteen():       # You just defined a function called twenty_eighteen, and it has two arguments(parameters): a and b
+def twenty_eighteen():       # You just defined a function called twenty_eighteen()
+    
     """Come up with the most creative expression that evaluates to 2018,
     using only numbers and the +, *, and - operators.
-    >>> twenty_eighteen() 
-    2018
-    """
+    # line 3 line 4 are docstring. It describes what we want for our output value in human language.
+    
+    >>> twenty_eighteen()    # This line is called doctest, it is the codes that should give a correct output which is described in the docstring. 
+    
+    2018                     # Here the line 4 should output 2018.
+    """                      
+                             # The doctest codes don't have arguments, so we can't add arguments in line 1.
     
     a = 2
     b = 1009
 
 
-    return a * b      # this is the function's output
+    return a * b             # this is the function's output. In this case, twenty_eighteen() could output 2018 which is what we want.
